6e3224c567331327c1a5e94adb01cf171e20566d
Lorenzo-69 <83821906+Lorenzo-69@users.noreply.github.com>
2021-07-29 11:30:45 +0200

    Update simplefs.h

--- simplefs.h
+++ simplefs.h
@@ -86,10 +86,12 @@ typedef struct {
   int pos_in_block;                // relative position of the cursor in the block
 } DirectoryHandle;
 
+//Stefano
 // initializes a file system on an already made disk
 // returns a handle to the top level directory stored in the first block
 DirectoryHandle* SimpleFS_init(SimpleFS* fs, DiskDriver* disk);
 
+//Lorenzo
 // creates the inital structures, the top level directory
 // has name "/" and its control block is in the first position
 // it also clears the bitmap of occupied blocks on the disk
@@ -97,47 +99,56 @@ DirectoryHandle* SimpleFS_init(SimpleFS* fs, DiskDriver* disk);
 // and set to the top level directory
 void SimpleFS_format(SimpleFS* fs);
 
+//Stefano
 // creates an empty file in the directory d
 // returns null on error (file existing, no free blocks)
 // an empty file consists only of a block of type FirstBlock
 FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename);
 
+//Lorenzo
 // reads in the (preallocated) blocks array, the name of all files in a directory 
 int SimpleFS_readDir(char** names, DirectoryHandle* d);
 
 
+//Stefano
 // opens a file in the  directory d. The file should be exisiting
 FileHandle* SimpleFS_openFile(DirectoryHandle* d, const char* filename);
 
-
+//Lorenzo
 // closes a file handle (destroyes it)
 int SimpleFS_close(FileHandle* f);
 
+//Stefano
 // writes in the file, at current position for size bytes stored in data
 // overwriting and allocating new space if necessary
 // returns the number of bytes written
 int SimpleFS_write(FileHandle* f, void* data, int size);
 
+//Lorenzo
 // writes in the file, at current position size bytes stored in data
 // overwriting and allocating new space if necessary
 // returns the number of bytes read
 int SimpleFS_read(FileHandle* f, void* data, int size);
 
+//Stefano
 // returns the number of bytes read (moving the current pointer to pos)
 // returns pos on success
 // -1 on error (file too short)
 int SimpleFS_seek(FileHandle* f, int pos);
 
+//Lorenzo
 // seeks for a directory in d. If dirname is equal to ".." it goes one level up
 // 0 on success, negative value on error
 // it does side effect on the provided handle
  int SimpleFS_changeDir(DirectoryHandle* d, char* dirname);
 
+//Stefano
 // creates a new directory in the current one (stored in fs->current_directory_block)
 // 0 on success
 // -1 on error
 int SimpleFS_mkDir(DirectoryHandle* d, char* dirname);
 
+//Lorenzo
 // removes the file in the current directory
 // returns -1 on failure 0 on success
 // if a directory, it removes recursively all contained files
