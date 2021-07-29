87f04f686137fbaf61cbf3f28b677e08191936a2
Stefano-25 <83822534+Stefano-25@users.noreply.github.com>
2021-07-29 22:22:56 +0200

    Create bitmap.c

---
+++ bitmap.c
@@ -0,0 +1,17 @@
+#include bitmap.h
+
+BitMapEntryKey BitMap_blockToIndex(int num) {
+  return null;
+}
+
+int BitMap_indexToBlock(int entry, uint8_t bit_num){
+  return 0;
+}
+
+int BitMap_get(BitMap* bmap, int start, int status){
+  return 0;
+}
+
+int BitMap_set(BitMap* bmap, int pos, int status){
+  return 0;
+}
