b97ea4692fe2a67a13e651ffdb0a1a22d8a0c398
Stefano-25 <83822534+Stefano-25@users.noreply.github.com>
2021-07-31 09:38:54 +0200

    implementazione funzione bitmapindextoblock

--- bitmap.c
+++ bitmap.c
@@ -1,17 +1,28 @@
-#include bitmap.h
+#include <stdlib.h>
+#include <stdio.h>
 
+#include "bitmap.h"
+
+// Lorenzo
 BitMapEntryKey BitMap_blockToIndex(int num) {
   return null;
 }
 
+// Stefano
 int BitMap_indexToBlock(int entry, uint8_t bit_num){
-  return 0;
+
+  if(bit_num < 0 || entry < 0) return -1;
+  int index = ( entry*8 ) + bit_num;
+  return index; 
+
 }
 
+// Lorenzo
 int BitMap_get(BitMap* bmap, int start, int status){
   return 0;
 }
 
+// Stefano
 int BitMap_set(BitMap* bmap, int pos, int status){
   return 0;
 }
