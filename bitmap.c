712039507e09b51d91c646502c7b2f2265075a98
Lorenzo-69 <83821906+Lorenzo-69@users.noreply.github.com>
2021-08-11 18:24:21 +0200

    bitmap completo

--- bitmap.c
+++ bitmap.c
@@ -1,49 +1,33 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include bitmap.h
 
-#include "bitmap.h"
-
-// Lorenzo
 BitMapEntryKey BitMap_blockToIndex(int num) {
-  return null;
+	int idx = num/8;
+	char off = num%8;
+	BitMapEntryKey entry = {
+		.entry_num = idx;
+		.bit_num = off;
+	}
+	return entry;
 }
 
-// Stefano
 int BitMap_indexToBlock(int entry, uint8_t bit_num){
-
-  if(bit_num < 0 || entry < 0) return -1;
-  int index = ( entry*8 ) + bit_num;
-  return index; 
-
-}
-
-// Lorenzo
-int BitMap_get(BitMap* bmap, int start, int status){
   return 0;
 }
 
-// Stefano
+int BitMap_get(BitMap* bmap, int start, int status){
+	if(start < 0 || start > bmap->num_bits) return -1;
+	
+	for(int i=start; i<bmap->num_bits; i++){
+		BitMapEntryKey entry = BitMap_blockToIndex(i);
+		int pos = entry->entry_num;
+		int value = (bmap->entries[pos] >> (8-entry->bit_num)) & 0x01;
+		//oppure value = bmap->entries[pos] << entry->bit_num & 0x80;
+		if(value == status) return i;
+	}
+	return -1;
+	
+}
+
 int BitMap_set(BitMap* bmap, int pos, int status){
-
-  if(pos < 0 || pos > bmap->num_bits) return -1;
-
-  BitMapEntryKey bitmapkey = BitMap_blockToIndex(pos);
-
-  char flag = 1 << bitmapkey.entry_num;  // controllo
-
-  char set = bmap->entries[bitmapkey.entry_num]; // settare
-
-  if(status) {
-
-    bmap->entries[bitmapkey.entry_num] = set | flag;
-
-    return set | flag;
-
-  }else {
-
-    bmap->entries[bitmapkey.entry_num] = set & (~flag);
-
-    return set & (~flag);
-
-  }
+  return 0;
 }
