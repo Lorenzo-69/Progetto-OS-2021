d64900ffce2dde123a901e60e4e1fa8628b1449e
Lorenzo-69 <quintilorenzo@gmail.com>
2021-09-01 16:46:11 +0200

    correzione errori sintassi bitmap.c

--- bitmap.c
+++ bitmap.c
@@ -1,12 +1,11 @@
-#include bitmap.h
+#include "bitmap.h"
 
 BitMapEntryKey BitMap_blockToIndex(int num) {
 	int idx = num/8;
 	char off = num%8;
-	BitMapEntryKey entry = {
-		.entry_num = idx;
-		.bit_num = off;
-	}
+	BitMapEntryKey entry;
+	entry.entry_num = idx;
+	entry.bit_num = off;
 	return entry;
 }
 
@@ -23,8 +22,8 @@ int BitMap_get(BitMap* bmap, int start, int status){
 	
 	for(int i=start; i<bmap->num_bits; i++){
 		BitMapEntryKey entry = BitMap_blockToIndex(i);
-		int pos = entry->entry_num;
-		int value = (bmap->entries[pos] >> (8-entry->bit_num)) & 0x01;
+		int pos = entry.entry_num;
+		int value = (bmap->entries[pos] >> (8-(entry.bit_num))) & 0x01;
 		//oppure value = bmap->entries[pos] << entry->bit_num & 0x80;
 		if(value == status) return i;
 	}
