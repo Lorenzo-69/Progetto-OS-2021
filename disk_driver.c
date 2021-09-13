5e60bf3290498be6f490cf5d0e65e517cd1ea09d
Stefano-25 <83822534+Stefano-25@users.noreply.github.com>
2021-09-13 22:25:39 +0200

    Fixed

--- disk_driver.c
+++ disk_driver.c
@@ -1,5 +1,6 @@
 #include "disk_driver.h"
 #include "bitmap.h"
+#include "simplefs.h"
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +15,7 @@ void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks){
   int fd;
   int bitmap_size = (num_blocks/8)+1;
   DiskHeader* disk_header = NULL;
-
-	if(disk == NULL || num_blocks <= 0){
+  if(disk == NULL || num_blocks < 1){
 		fprintf(stderr,"Errore: Input non valido DiskDriver_init. \n");
 		return;
 	}
@@ -25,14 +25,14 @@ void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks){
     fd = open(filename,O_RDWR,(mode_t)0666);
 
     if(fd == -1){
-			fprintf(stderr,"Errore: Impossibile aprire il file DIskDriver_init. \n");
-			return;
+			fprintf(stderr,"Errore: Impossibile aprire il file DiskDriver_init\n");
+			exit(-1);
 		}
 
     disk_header = (DiskHeader*) mmap(0, sizeof(DiskHeader)+bitmap_size, PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
     if(disk_header == MAP_FAILED){
-		  fprintf(stderr,"Errore: mmap fallito DIskDriver_init \n");
-      return;
+		  fprintf(stderr,"Errore: mmap fallita DiskDriver_init \n");
+      exit(-1);
     }
 
 		disk->header = disk_header;
@@ -44,21 +44,21 @@ void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks){
     fd = open(filename, O_RDWR|O_CREAT|O_TRUNC,(mode_t)0666);
 
     if(fd == -1){
-			fprintf(stderr,"Errore: Impossibile aprire il file DiskDriver_init");
-			return;
+			fprintf(stderr,"Errore: Impossibile aprire il file DIskDriver_init\n");
+			exit(-1);
 		}
     if(posix_fallocate(fd,0,sizeof(DiskHeader)+bitmap_size) > 0){
-      fprintf(stderr,"Errore posix f-allocate");
+      fprintf(stderr,"Errore posix f-allocate DIskDriver_init\n");
       close(fd);
-      return;
+      exit(-1);
     }
     disk_header = (DiskHeader*) mmap(0, sizeof(DiskHeader)+bitmap_size, PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
     if(disk_header == MAP_FAILED){
-		  fprintf(stderr,"Error: mmap failed \n");
-      close(fd);
-      return;
+		  fprintf(stderr,"Erroer: mmap fallita DIskDriver_init\n");
+      exit(-1);
     }
     disk->header = disk_header;
+
     disk->bitmap_data = (char*)disk_header + sizeof(DiskHeader);
     disk_header->num_blocks = num_blocks;
     disk_header->bitmap_blocks = num_blocks;
@@ -75,35 +75,39 @@ void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks){
 
 int DiskDriver_readBlock(DiskDriver* disk, void* dest, int block_num,int size){
   if(block_num >= disk->header->bitmap_blocks || block_num < 0 || dest == NULL || disk == NULL){
-    fprintf(stderr,"Errore: Input non valido DiskDriver_readBlock\n");
+		fprintf(stderr,"Errore: Input non valido DiskDriver_readBlock \n");
     return -1;
-  } 
+	}
+	
   BitMap bitmap;
   bitmap.num_bits = disk->header->bitmap_blocks;
   bitmap.entries = disk->bitmap_data;
 
   if(BitMap_get(&bitmap, block_num, 0) == block_num){
-    fprintf(stderr,"Errore: Blocco vuoto DiskDriver_readBlock\n");
     return -1;
   } 
   int fd = disk->fd;
     
 	off_t offset = lseek(fd, sizeof(DiskHeader)+disk->header->bitmap_entries+(block_num*BLOCK_SIZE), SEEK_SET);	
 	if(offset == -1){
-		fprintf(stderr,"Errore: lseek DiskDriver_readBlock\n");
+		fprintf(stderr,"Error: lseek");
 		return -1;
 	}
 
-	int ret, readb = 0;
-	while(readb < size){																	
-		if((ret = read(fd, dest + readb, size - readb)) == -1){
-			if(ret == -1){
-        fprintf(stderr,"Errore: read DiskDriver_readBlock\n");
-        return -1;
-      }
+	int ret, read_bytes = 0;
+	while(read_bytes < size){																	
+		if((ret = read(fd, dest + read_bytes, size - read_bytes)) == -1){
+			if(errno == EINTR) 
+				continue;
+			else{
+				fprintf(stderr,"Errore: read DiskDriver_readBlock\n");
+				return -1;
+			}
 		}
-		readb += ret;						
+			
+	read_bytes += ret;						
 	}
+
   return 0;
 }
 
@@ -125,29 +129,62 @@ int DiskDriver_writeBlock(DiskDriver* disk, void* src, int block_num, int size){
   fprintf(stderr,"Errore: impossibile blocco occupato DiskDriver_writeblock\n");
   return -1;
  }else{
+   disk->header->free_blocks--;
    if(BitMap_set(&bmap, block_num, 1) < 0){
-    fprintf(stderr,"Errore: errore set bitmap DiskDriver_writeBlock\n");
+    fprintf(stderr,"Errore: set bitmap DiskDriver_writeBlock");
     return -1;
    } 
-    int written = 0;
-    off_t offset = lseek(fd,sizeof(DiskHeader) + disk->header->bitmap_entries + (block_num*BLOCK_SIZE),SEEK_SET);
-    if(offset==-1){
-      fprintf(stderr,"Errore: errore lseek DiskDriver_writeBlock\n");
-      return -1;
-    }
-    while(written < size){
-      ret = write(fd,src+written,size-written);
-      if(ret == -1){
-        fprintf(stderr,"Errore: errore write DiskDriver_writeBlock\n");
+   int written = 0;
+   off_t offset = lseek(fd,sizeof(DiskHeader) + disk->header->bitmap_entries + (block_num*BLOCK_SIZE),SEEK_SET);
+   if(offset==-1){
+    fprintf(stderr,"Errore: DiskDriver_writeBlock\n");
+    return -1;
+   }
+   while(written < size){
+    ret = write(fd,src+written,size-written);
+    if(ret == -1){
+      fprintf(stderr,"Errore: write DiskDriver_writeBlock()\n");
         return -1;
-      }
+      } 
       written += ret;
+
     }
-    disk->header->free_blocks--;
-  }
-  disk->header->first_free_block = DiskDriver_getFreeBlock(disk, 0);
+ }
+ disk->header->first_free_block = DiskDriver_getFreeBlock(disk, 0);
+ return 0;
+}
+
+
+int DiskDriver_updateBlock(DiskDriver* disk, void* src, int block_num, int bytes_to_write){
+	if(block_num >= disk->header->bitmap_blocks || block_num < 0 || src == NULL || disk == NULL ){
+		fprintf(stderr,"Errore: Impossibile aggiornare il blocco DiskDriver_updateBlock \n");
+    return -1;
+	}
+  int fd = disk->fd;
+    
+	off_t offset = lseek(fd, sizeof(DiskHeader)+disk->header->bitmap_entries+(block_num*BLOCK_SIZE), SEEK_SET);	
+	if(offset == -1){
+		fprintf(stderr,"Errore: lseek DiskDriver_updateBlock\n");
+		return -1;
+	}
+		
+	int ret, written_bytes = 0;
+	while(written_bytes < bytes_to_write){																		
+		if((ret = write(fd, src + written_bytes, bytes_to_write - written_bytes)) == -1){
+			if(errno == EINTR) 
+				continue;
+			else{
+				fprintf(stderr,"Errore: write DiskDriver_updateBlock\n");
+				return -1;
+			}
+		}
+			
+		written_bytes += ret;						
+	}
+
   return 0;
 }
+
 int DiskDriver_freeBlock(DiskDriver* disk, int block_num){
   //controllo input
   if(block_num > disk->header->num_blocks){
@@ -177,35 +214,10 @@ int DiskDriver_freeBlock(DiskDriver* disk, int block_num){
   return 0;
 }
 
-int DiskDriver_updateBlock(DiskDriver* disk, void* src, int block_num, int size){
-	if(block_num >= disk->header->bitmap_blocks || block_num < 0 || src == NULL || disk == NULL ){
-		fprintf(stderr,"Errore: impossibile aggiornare il blocco DiskDriver_updateBlock\n");
-    return -1;
-	}
-  int fd = disk->fd;
-    
-	off_t offset = lseek(fd, sizeof(DiskHeader)+disk->header->bitmap_entries+(block_num*BLOCK_SIZE), SEEK_SET);	
-	if(offset == -1){
-		fprintf(stderr,"Errore: lseek DiskDriver_updateBlock\n");
-		return -1;
-	}
-		
-	int ret, written = 0;
-	while(written < size){																		
-		ret = write(fd,src+written,size-written);
-      if(ret == -1){
-        fprintf(stderr,"Errore: errore write DiskDriver_updateBlock\n");
-        return -1;
-      }
-      written += ret;					
-	}
-  return 0;
-}
-
 int DiskDriver_getFreeBlock(DiskDriver* disk, int start){
   //controllo input
   if ((disk == NULL)){
-    fprintf(stderr, "Errore: Disco non valido DIskDriver_getFreeBlock. \n");
+    fprintf(stderr, "Errore: Disco non valido DiskDriver_getfreeBlock. \n");
     return -1;
   }
   //preparo bitmap
@@ -214,7 +226,7 @@ int DiskDriver_getFreeBlock(DiskDriver* disk, int start){
   bmap.entries = disk->bitmap_data;
   //altro controllo input
   if ((start < 0) || (start > disk->header->num_blocks)){
-    fprintf(stderr, "Errore: Punto d'inizio non valido DIskDriver_getFreeBlock. \n");
+    fprintf(stderr, "Errore: Punto d'inizio non valido DiskDriver_getfreeBlock. \n");
     return -1;
   }
   return BitMap_get(&bmap, start, 0);
