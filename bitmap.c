082dee408c7ff7a0328a25877a77f3fd7defe112
Stefano-25 <83822534+Stefano-25@users.noreply.github.com>
2021-09-13 17:50:41 +0200

    Fixed problem bitmap_set & bitmap_get

--- bitmap.c
+++ bitmap.c
@@ -20,20 +20,20 @@ int BitMap_indexToBlock(int entry, uint8_t bit_num){
 
 int BitMap_get(BitMap* bmap, int start, int status){
 	if(start < 0 || start > bmap->num_bits) return -1;
-	
-	for(int i=start; i<bmap->num_bits; i++){
+
+	int i;
+
+	for(i = start; i < bmap->num_bits; i++) {
 		BitMapEntryKey entry = BitMap_blockToIndex(i);
 		int pos = entry.entry_num;
-		//int value = (bmap->entries[pos] >> ((8-entry.bit_num))) & 0x01;
-		//oppure 
-		//int value = bmap->entries[pos] << entry.bit_num & 0x80;
-		int value = (bmap->entries[pos] & (1<<(7-entry.bit_num)));
-		if(status == 1){
+
+	 	int value = (bmap->entries[pos] & (1 << (7 - entry.bit_num))); 
+
+		if(status == 1) {
 			if(value > 0) return i;
 		}else{
 			if(value == 0) return i;
 		}
-		//if(value == status) return i;
 	}
 	return -1;
 	
@@ -42,36 +42,15 @@ int BitMap_get(BitMap* bmap, int start, int status){
 int BitMap_set(BitMap* bmap, int pos, int status){
 
 
-  if(pos < 0 || pos > bmap->num_bits) return -1;
+	if(pos < 0 || pos > bmap->num_bits) return -1;
 
-  BitMapEntryKey bitmapkey = BitMap_blockToIndex(pos);
+	BitMapEntryKey bitmapkey = BitMap_blockToIndex(pos);
 
-  char flag = 1 << bitmapkey.entry_num;  // controllo
-
-  char set = bmap->entries[bitmapkey.entry_num]; // settare
-
-  uint8_t val = 1<< (7-bitmapkey.bit_num);
-
-  if(status) {
-	
-	
-	bmap->entries[bitmapkey.entry_num] |= val;
-
-	return 0;
-
-    //bmap->entries[bitmapkey.entry_num] = set | flag;
-
-    //return set | flag;
-
-  }else {
-
-	
-	bmap->entries[bitmapkey.entry_num] &= (~val);
-	return 0;
-
-   // bmap->entries[bitmapkey.entry_num] = set & (~flag);
-
-    //return set & (~flag);
-
-  }
+	uint8_t val = 1 << (7 - bitmapkey.bit_num);
+	if(status){
+		bmap->entries[bitmapkey.entry_num] |= val;
+  	}else{
+   		bmap->entries[bitmapkey.entry_num] &= ~(val);
+    }
+    return status;
 }
